4de98d39b379863fcf4816a5cb3312a2b9a7bd7b
ekgus419 <ekgus419@gmail.com>
2023-03-05 16:09:27 +0900

    modify

--- study/src/main/java/com/example/controller/ProductServiceController.java
+++ study/src/main/java/com/example/controller/ProductServiceController.java
@@ -2,14 +2,9 @@ package com.example.controller;
 
 import com.example.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.example.model.Product;
-
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 public class ProductServiceController {
@@ -56,29 +51,29 @@ public class ProductServiceController {
     }
     */
 
-    @GetMapping("/product")
+    @GetMapping("/products")
     public ResponseEntity readProducts() {
         return ResponseEntity.ok(productService.readProductAll());
     }
 
-    @GetMapping("/product/{id}")
+    @GetMapping("/products/{id}")
     public ResponseEntity<Product> readProduct (@PathVariable Long id) {
         return ResponseEntity.ok(productService.readProduct(id));
     }
 
-    @PostMapping("/product/save")
+    @PostMapping("/products")
     public ResponseEntity<Product> createProduct (@RequestBody Product product) {
         return ResponseEntity.ok(productService.saveProduct(product));
     }
 
-    @PutMapping("/product/update")
+    @PutMapping("/products")
     public ResponseEntity<Product> updateProduct (@RequestBody Product product) {
         Product newProduct = productService.readProduct(product.getId());
         newProduct.setName(product.getName());
         return ResponseEntity.ok(productService.saveProduct(newProduct));
     }
 
-    @DeleteMapping("/product/{id}")
+    @DeleteMapping("/products/{id}")
     public ResponseEntity<Void> deleteProduct (@PathVariable Long id) {
         productService.deleteProduct(id);
         return ResponseEntity.ok().build();
